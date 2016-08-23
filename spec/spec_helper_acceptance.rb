c13d682a56cb3296930c5f216b716eba7ff72f9d
Matt Dainty <matt@bodgit-n-scarper.com>
2016-08-23 12:00:51 +0100

    Pull in bodgitlib for validation

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -15,6 +15,7 @@ RSpec.configure do |c|
       puppet_module_install(:source => proj_root, :module_name => 'avahi')
       on host, puppet('module','install','puppetlabs-stdlib'),   { :acceptable_exit_codes => [0,1] }
       on host, puppet('module','install','puppetlabs-firewall'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module','install','bodgit-bodgitlib'),    { :acceptable_exit_codes => [0,1] }
       on host, puppet('module','install','bodgit-dbus'),         { :acceptable_exit_codes => [0,1] }
     end
   end
